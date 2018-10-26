85f23e8faa00e16a732e27d8b4744ed46bae91ca
cycs <saenen.chris@outlook.com>
2018-10-26 12:55:08 +0200

    Update app/Soil.php

---
+++ app/Soil.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Soil extends Model {
+    protected $fillable = ['name', 'address', 'granulometry', 'ph', 'humus_percent'];
+}
