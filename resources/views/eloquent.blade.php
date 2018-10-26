8672cdb0e65bf80c8495e904518cde9d9154cd16
cycs <saenen.chris@outlook.com>
2018-10-26 18:15:47 +0200

    Update resources/views/eloquent.blade.php

--- resources/views/eloquent.blade.php
+++ resources/views/eloquent.blade.php
@@ -2,25 +2,17 @@
 
 
 @section('content')
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Danger level</th>
-        </tr>
-    @foreach($bears as $bear)
-        <tr>
-            <td>{{ $bear->name }}</td>
-            <td>{{ $bear->type }}</td>
-            <td>{{ $bear->danger_level }}</td>
-        </tr>
-    @endforeach
-    @foreach($seeds as $seed)
-            {{$seed->name}}
 
+    @foreach($seeds as $seed)
+        <li>
+            <ul>
+                <li>{{$seed->name}}</li>
+                <li>
             @foreach($seed->illnesses as $illness)
                 {{$illness->name}}
             @endforeach
+                </li>
+            </ul>
+        </li>
     @endforeach
-    </table>
 @endsection
