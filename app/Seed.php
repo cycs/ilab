42b56d2f74d0c198e8c3273fb423ddbda7cfce03
cycs <saenen.chris@outlook.com>
2018-10-26 12:55:00 +0200

    Update app/Seed.php

---
+++ app/Seed.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Seed extends Model {
+    protected $fillable = [
+        'name',
+        'latin_name',
+        'variety',
+        'type',
+        'exposure',
+        'water',
+        'fertilizer',
+        'planting_period',
+        'harvest_period',
+        'grown_img_url',
+        'seed_img_url',
+        'price',
+        'difficulty',
+        'tutorial',
+        'advices',
+        'soil_ph',
+        'soil_humus_percent',
+        'soil_granulometry'
+    ];
+
+    public function illnesses(){
+        return $this->belongsToMany('App\Illness', 'seeds_illnesses', 'seed_id', 'illness_id');
+    }
+    public function soils(){
+        return $this->belongsTo('App\Soil', 'seeds_soils', 'seed_id', 'soil_id');
+    }
+}
