4ea0ea68c7828b9bcb37932768fa869997011f55
cycs <saenen.chris@outlook.com>
2018-10-26 12:56:02 +0200

    Update database/migrations/2018_10_26_080934_create_soil_table.php

---
+++ database/migrations/2018_10_26_080934_create_soil_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSoilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soils', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+            $table->string('address');
+            $table->enum('granulometry', ['argile', 'limons fins', 'limons grossiers', 'sables fins', 'sables grossiers', 'calcaire', 'humifère']);
+            $table->tinyInteger('ph');
+            $table->tinyInteger('humus_percent');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soils');
+    }
+}
