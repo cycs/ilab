07a9df021e14287c9a6280c81840d35f87ce514e
cycs <saenen.chris@outlook.com>
2018-10-26 16:55:46 +0200

    Update database/seeds/PermacultureTableSeeder.php

---
+++ database/seeds/PermacultureTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Seed;
+use App\Illness;
+use App\Soil;
+
+class PermacultureTableSeeder extends Seeder
+{
+    /**
+     * Run the database seed.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+
+    }
+}
