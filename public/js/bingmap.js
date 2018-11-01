ac1f8b2761512004631d58617978a79f7ac81647
cycs <saenen.chris@outlook.com>
2018-11-01 15:58:06 +0100

    Update public/js/bingmap.js

--- public/js/bingmap.js
+++ public/js/bingmap.js
@@ -1,13 +1,25 @@
 var map;
 function GetMap() {
     console.log("get map");
+
+    var bingStyle = {
+      "elements": {
+          "road" : { "fillColor": "#b892db" },
+      }
+    };
+
     map = new Microsoft.Maps.Map('#myMap', {
         credentials: 'AtubFyYmW6LqLX9baFR4eYZB61CYObCVHAaa-33fRvhwVhRwEXnsKijb_3a5yDgM',
         center: new Microsoft.Maps.Location(50.5011, 4.465),
+        customMapStyle : bingStyle,
         mapTypeId: Microsoft.Maps.MapTypeId.grayscale,
         zoom: 7,
         showDashboard: false
     });
+
+    map.setView({ labelOverlay: Microsoft.Maps.LabelOverlay.hidden});
+    // map.Polygon
+    // console.log(Microsoft.Maps);
 
     var soils = fetch('./api/soils').then(response => response.json()).then(display);
     function display(edenias){
