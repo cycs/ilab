948fe9b3350176681ea1499fe774cf098f2e411e
ManonMirguet <mirguetmanon8@gmail.com>
2018-11-04 17:01:32 +0100

    update files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <!-- HAMBURGER -->
+    <div class="hamb">
+        <div class="hamb__bar hamb__bar--big"></div>
+        <div class="hamb__bar hamb__bar--small"></div>
+        <div class="hamb__bar"></div>
+    </div>
+    <!--<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    You are logged in!
+                        You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
+    </div> -->
+
+
+
+    <div class="container">
     </div>
-</div>
+
+
+
 @endsection
