b9bac825316e4dfe878e2cf881a38ac556e0c6e0
cycs <saenen.chris@outlook.com>
2018-11-05 17:09:00 +0100

    update files

--- resources/views/eloquent.blade.php
+++ resources/views/eloquent.blade.php
@@ -1,18 +1,10 @@
 @extends('layouts.app')
 
 
-@section('content')
 
-    @foreach($seeds as $seed)
-        <li>
-            <ul>
-                <li>{{$seed->name}}</li>
-                <li>
-            @foreach($seed->illnesses as $illness)
-                {{$illness->name}}
-            @endforeach
-                </li>
-            </ul>
-        </li>
-    @endforeach
-@endsection
+
+    @if(Auth::user())
+        {{Auth::user()->email}}
+    @else
+        <h1>Hallo! {{ var_dump(Auth::check())}}</h1>
+    @endif
