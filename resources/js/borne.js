460dec2fced3775d1d9b64fdbc3784690c00d384
ManonMirguet <mirguetmanon8@gmail.com>
2018-11-07 15:55:24 +0100

    coucou

--- resources/js/borne.js
+++ resources/js/borne.js
@@ -4,6 +4,6 @@ var hamb = document.querySelector(".hamb");
 var nav = document.querySelector('.nav');
 
 hamb.addEventListener('click', function(e){
-    console.log(e);
+    console.info('salut');
     nav.classList.toggle('hide');
 });
