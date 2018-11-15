b8e341a94b1bb13e7324f57329dd6b5af1d6e5d7
cycs <saenen.chris@outlook.com>
2018-11-16 00:18:36 +0100

    metas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,4 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-//Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-/*const app = new Vue({
-    el: '#app'
-});*/
-
-
-console.log('coucou manon, c\'est ici que tu codes');
-console.log('PAS TOUCHE BILLY');
-console.log('Pour toi, c\'est facile Manon, app.js est déjà load sur tes pages, normalement ça fonctionne');
-
 // BORNE HAMBURGER
 
 var hamb = document.querySelector(".menu");
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,21 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="apple-mobile-web-app-capable" content="yes">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Edenia</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}/">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('/site.webmanifest')}}">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta property="og:image:height" content="459">
+    <meta property="og:image:width" content="877">
+    <meta property="og:title" content="Edenia">
+    <meta property="og:description" content="Les potagers collectifs connect&eacute;s">
+    <meta property="og:url" content="http://www.edenia.be">
+    <meta property="og:image" content="{{asset('/social/og-image.jpg')}}">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
